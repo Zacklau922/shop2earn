662b6f29d61036b0980d9d27af44d39f742aceca
zacklau <zacktah@gmail.com>
2023-10-13 16:56:03 +0800

    remove margin-y from main layout

--- app/(root-client)/layout.tsx
+++ app/(root-client)/layout.tsx
@@ -4,7 +4,7 @@ import Navbartabs from "@/components/navbar/NavbarTabs";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="container my-3 flex max-w-md flex-col overflow-hidden rounded-3xl border-1 border-gray-100 bg-slate-100 shadow-2xl supports-[height:100cqh]:h-[100cqh] supports-[height:100svh]:h-[100svh]">
+    <main className="container flex max-w-md flex-col overflow-hidden rounded-3xl border-1 border-gray-100 bg-slate-100 shadow-2xl supports-[height:100cqh]:h-[100cqh] supports-[height:100svh]:h-[100svh]">
       <NavbarClient />
       <div className="flex-1 overflow-auto scrollbar-hide">{children}</div>
       <Navbartabs />
