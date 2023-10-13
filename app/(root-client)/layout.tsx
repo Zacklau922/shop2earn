68a4d120f718e7ac3b895ba6716768f824d1902d
zacklau <zacktah@gmail.com>
2023-10-13 10:37:41 +0800

    mobile view fixing

--- app/(root-client)/layout.tsx
+++ app/(root-client)/layout.tsx
@@ -4,7 +4,7 @@ import { NavbarClient } from "@/components/navbar/NavbarClient";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="container my-3 flex h-screen max-w-sm flex-col rounded-3xl border-1 border-gray-100 shadow-xl">
+    <main className="container flex min-h-[100svh] max-w-sm flex-col rounded-3xl border-1 border-gray-100 shadow-xl">
       <NavbarClient />
       <div className="flex-1 overflow-auto">{children}</div>
       <Navbartabs />
--- components/navbar/NavbarTabs.tsx
+++ components/navbar/NavbarTabs.tsx
@@ -16,7 +16,7 @@ const Navbartabs: React.FC = () => {
   const tabKey = pathname.replace("/", "");
 
   return (
-    <div className="flex w-full items-center justify-center">
+    <div className="flex w-full items-center justify-center pb-3">
       <Tabs
         selectedKey={tabKey}
         aria-label="Options"
