642d1f89b8dc4a6d75a7cb96db77fefbb8e03c03
zacklau <zacktah@gmail.com>
2023-10-21 21:55:01 +0800

    remove setTimeout, setInterval

--- app/(root-client)/app/[id]/menu/layout.tsx
+++ app/(root-client)/app/[id]/menu/layout.tsx
@@ -1,34 +1,13 @@
-"use client";
-import Loading from "@/app/components/loading";
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 export default function MenuLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulating a fetch or some async operation
-    // You can replace this with an actual API call or data fetching logic
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 1000); // This will simulate a 1-second loading time. Adjust as needed.
-
-    // Clean up the timeout if the component is unmounted before the timeout finishes
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <section className="flex h-screen flex-col items-center p-3">
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <div className="inline-block justify-center text-center">
-          {children}
-        </div>
-      )}
+      <div className="inline-block justify-center text-center">{children}</div>
     </section>
   );
 }
